ceabc81f77f05a83ce94a1725ac82201e2a2b2df
duycc <gz_duyong@163.com>
2022-03-30 22:49:09 +0800

    review basic

---
+++ Ch01-Python核心编程/01_Python基础语法/01-基础语法/03-认识bug.py
@@ -0,0 +1,4 @@
+
+# 定义变量：存储数据TOM
+my_name = 'TOM'
+print(my_name)
---
+++ Ch01-Python核心编程/01_Python基础语法/03-循环/hm_13_break控制for循环.py
@@ -0,0 +1,7 @@
+
+str1 = 'itheima'
+for i in str1:
+    # 当某些条件成立退出循环 -- break：条件 i取到字符e
+    if i == 'e':
+        break
+    print(i)
