28c1ee7aefab2826bbe9f47586f6944829f343c7
duycc <gz_duyong@163.com>
2023-04-13 19:02:01 +0800

    2023-04-13

---
+++ Ch07-NLP自然语言处理/02-pytorch/04_使用TorchAPI完成线性回归.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+from torch import optim
+import numpy as np
+from matplotlib import pyplot as plt
+
+# 1. 定义数据
+x = torch.rand([50, 1])
+y = x * 3 + 0.8
+
+
+# 2. 定义模型
+class Lr(nn.Module):
+    def __init__(self):
+        super(Lr, self).__init__()
+        self.linear = nn.Linear(1, 1)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+
+model = Lr()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=1e-3)
+
+# 3. 训练模型
+for i in range(30000):
+    out = model(x)
+    loss = criterion(y, out)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if (i + 1) % 20 == 0:
+        print("Epoch[{}/{}] loss={:.6f}".format(i, 30000, loss.data))
+
+
+# 4. 模型评估
+model.eval()
+predict = model(x)
+predict = predict.data.numpy()
+plt.scatter(x.data.numpy(), y.data.numpy(), c="r")
+plt.plot(x.data.numpy(), predict)
+plt.show()
