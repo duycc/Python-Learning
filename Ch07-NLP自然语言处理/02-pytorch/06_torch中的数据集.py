aff1ce5b5e28246e4e162c93e651c453f7e2a565
duycc <gz_duyong@163.com>
2023-04-14 15:16:21 +0800

    2023-04-14

---
+++ Ch07-NLP自然语言处理/02-pytorch/06_torch中的数据集.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+data_path = "data/SMSSpamCollection"
+
+
+class CifarDataset(Dataset):
+    def __init__(self):
+        lines = open(data_path, "r")
+        lines = [[l[:4].strip(), l[4:].strip()] for l in lines]
+        self.df = pd.DataFrame(lines, columns=["label", "sms"])
+
+    def __getitem__(self, index):
+        single_item = self.df.iloc[index, :]
+        return single_item.values[0], single_item.values[1]
+
+    def __len__(self):
+        return self.df.shape[0]
+
+
+# d = CifarDataset()
+# for i in range(len(d)):
+#     print(i, d[i])
+
+
+dataset = CifarDataset()
+data_loader = DataLoader(dataset, batch_size=128, shuffle=True)
+
+for idx, (label, context) in enumerate(data_loader):
+    print(idx, label, context)
+    print("*" * 100)
